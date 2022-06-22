6f58dc028afd3cf8bc96fe3bc16513c9d3643a1f
hariprasad <hariprasadcsmails@gmail.com>
2022-06-22 21:37:14 +0530

    minor improvemnt in macos script

--- configs/os/mac_x64.sh
+++ configs/os/mac_x64.sh
@@ -110,11 +110,14 @@ alias mv='mv -i'
 alias cp='cp -i'
 alias mkdir='mkdir -pv'
 
+return
 
 # iterm2 shell integration https://iterm2.com/documentation-shell-integration.html
 if [ ! -f "${HOME}/.iterm2_shell_integration.zsh" ]; then
   echo "> Installing iterm2 shell integration"
   curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | zsh
+else
+  test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 fi
 
 # Install fonts
@@ -125,7 +128,6 @@ for file in $DOTFILES_PATH/etc/fonts/*/*; do
   fi
 done
 
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
 
 
