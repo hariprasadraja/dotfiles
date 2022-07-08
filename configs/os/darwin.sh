15a663a32d2a6236989fd21c9b0355fd579ca58d
hariprasad <hariprasadcsmails@gmail.com>
2022-07-08 09:49:07 +0530

    imporved script to install fonts

--- configs/os/darwin.sh
+++ configs/os/darwin.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # Install fonts
 # Hold it, it takes more time to setup
-fonts=('FiraCode' 'DaddyTimeMono' 'SourceCodePro' 'CascadiaCode')
+fonts=('FiraCode.zip' 'DaddyTimeMono.zip' 'SourceCodePro.zip' 'CascadiaCode.zip')
 font_dir=~/Library/Fonts
-newfont=false
+[ ! -d $font_dir ] && mkdir -p $font_dir
 for font in $fonts; do
-  if [ ! -f "$font_dir/$font.zip" ]; then
-    echo "Installing $font_dir/$font.zip"
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
-    cwd=$PWD
-    mv $font.zip $font_dir && cd $font_dir && unzip $font_dir/$font.zip && cd $cwd
-    newfonts=true
+  if [ ! -f "$font_dir/$font" ]; then
+   URL_LIST+=("https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font")
   fi
 done
 
-if [ "$newfonts" = "true" ]; then
+if [ ${#URL_LIST[@]} > 0 ]; then
+  cwd=$PWD && cd $font_dir
+  echo $URL_LIST | xargs -n 1 -P 4 wget
+  echo $fonts | xargs -n 1 -P 4 unzip -f
+  cd $cwd
   sudo atsutil databases -remove && atsutil server -shutdown && atsutil server -ping
 fi
 
--- configs/os/linux.sh
+++ configs/os/linux.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 # Install fonts
-fonts=('FiraCode' 'DaddyTimeMono' 'SourceCodePro' 'CascadiaCode')
-font_dir=/usr/local/share/fonts
+fonts=('FiraCode.zip' 'DaddyTimeMono.zip' 'SourceCodePro.zip' 'CascadiaCode.zip')
+font_dir=~/.local/share/fonts
+[ ! -d $font_dir ] && mkdir -p $font_dir
 for font in $fonts; do
-  if [ ! -f "$font_dir/$font.zip" ]; then
-    echo "Installing $font_dir/$font.zip"
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
-    cwd=$PWD
-    mv $font.zip $font_dir && cd $font_dir && unzip $font_dir/$font.zip && cd $cwd
-    fc-cache -f -v
+  if [ ! -f "$font_dir/$font" ]; then
+   URL_LIST+=("https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font")
   fi
 done
+
+if [ ${#URL_LIST[@]} > 0 ]; then
+  cwd=$PWD && cd $font_dir
+  echo $URL_LIST | xargs -n 1 -P 4 wget
+  echo $fonts | xargs -n 1 -P 4 unzip -f
+  cd $cwd
+  fc-cache -f -v
+fi
